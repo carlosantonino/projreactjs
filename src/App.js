81c6980ddf9c6175cf019af9db0a236ee8e2abd3
Carlos <72629502+carlosantonino@users.noreply.github.com>
2023-01-22 20:17:00 -0300

    instalando projeto react

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Inicio</h1>
+      <p>Isso é uma página inicial do React JS</p>
+      
+    </div>
+  );
+}
+
+export default App;
